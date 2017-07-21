367cc70209c89f7bdc03dc771c379c5e3c43e602
iamjpy <jj2739@gmail.com>
2017-07-21 13:20:55 -0400

    Major refactoring and bug fixes complete

--- README.md
+++ README.md
@@ -31,23 +31,4 @@ Using photos and information that the API provides, you’ll display 12 users, a
 - [x] Add a way to filter the directory by name or username. To do this, you’ll need to request a random user nationality that will only return data in the English alphabet. Note: you don't have to rely on the API to return search results. You'll need to write functionality that filters results once they already on the page.
 - [x] Add a way to move back and forth between employee detail windows when the modal window is open.
 
-## Make sure your program is free of syntax errors.
-- [] You can monitor any errors by looking at the Developer Tools console in your browser.
-- [] Use jsHint (see Resources links) to check your code for syntax and formatting problems. (Installed as an Atom.io plugin)
-
-## Commenting
-- [] Make sure you add code comments to explain how your programming works.
-
-## Peer Review
-- [] Before you submit your project for review, make sure you can check off all of the items on the Student Project Submission Checklist. The checklist is designed to help you make sure you’ve met the grading requirements and that your project is complete and ready to be submitted!
-
-## Browser Compatibility
-Check project for cross browser compatibility in multiple browsers (at least three):
-- [] Google Chrome Version 58.0.3029.96 (64-bit)
-- [] Mozilla Firefox 53.0.2 (64-bit)
-- [] Internet Explorer/Edge
-  - Tested in IE11 on Windows 7 in Virtual Box (using Modern.ie)
-  - Tested in Edge on Windows 10 in Virtual Box (using Modern.ie)
-- [] Safari Version 10.1 (12603.1.30.0.34)
-
 # Feedback
--- js/script.js
+++ js/script.js
@@ -1,99 +1,114 @@
 $(document).ready(function() {
 
-  let employeeList = {};
-  
-  // Call to Random User Generator API to generate 12 employees
-  $.ajax({
-    url: 'https://randomuser.me/api/?results=12&seed=coffee&nat=us',
-    dataType: 'json',
-    success: function(data) {
-      employeeList = data.results;
-      let profile ='';
-      let modal = '';
+  let employeeList = [];
 
-      $.each(data.results, function(i, employee) {
-        profile += '<td class="employee-profile" id=' + i + '>';
-        profile += '<a href="#openModal' + i + '">';
-        profile += '<img src="' + employee.picture.large + '" class="employee-picture">';
-        profile += '<div class="employee-info">';
-        profile += '<p class="name">' + employee.name.first + ' ' + employee.name.last + '</p>';
-        profile += '<p class="email">' + employee.email + '</p>';
-        profile += '<p class="city">' + employee.location.city + '</p>';
-        profile += '</div></a></td>';
-      });
+  // Get employee data results from API call to Random User Generator and display directory
+  let getEmployeeData = (employeeData) => {
+    employeeList = employeeData;
+    displayEmployeeDirectory(employeeList); // Call function to display employeeList data
+  }
 
-      $('.employees').html(profile);
+  // Display current listing of employees in the main employee directory
+  let displayEmployeeDirectory = (list) => {
+    let profile ='';
 
-      $.each(data.results, function(i, employee) {
+    $.each(list, function(i, employee) {
+      profile += '<td class="employee-profile" id=' + i + '>';
+      profile += '<a href="#openModal' + i + '">';
+      profile += '<img src="' + employee.picture.large + '" class="employee-picture">';
+      profile += '<div class="employee-info">';
+      profile += '<p class="name">' + employee.name.first + ' ' + employee.name.last + '</p>';
+      profile += '<p class="email">' + employee.email + '</p>';
+      profile += '<p class="city">' + employee.location.city + '</p>';
+      profile += '</div></a></td>';
+    });
 
-        // Format Birthdate
-        let dobString = employee.dob.split('-');
-        let dobDay = dobString[2].substring(0, 2);
-        let birthday = 'Birthday: ' + dobString[1] + '/' + dobDay + '/' + dobString[0];
+    $('.employees').html(profile);
+    loadModals(list); // Call function to load/update modal windows with employee data
+  }
 
-        modal += '<div id="openModal' + i + '" class="modalDialog"><div>';
-        modal += '<img src="' + employee.picture.large + '" class="employee-picture-details">';
-        modal += '<div>';
-        modal += '<p class="name">' + employee.name.first + ' ' + employee.name.last + '</p>';
-        modal += '<p class="email">' + employee.email + '</p>';
-        modal += '<p class="city">' + employee.location.city + '</p><hr>';
-        modal += '<p>' + employee.cell + '</p>';
-        modal += '<p>' + employee.location.street + ' ' + employee.location.city + ', ' + employee.location.state + ' ' + employee.location.postcode + '</p>';
-        modal += '<p>' + birthday + '</p>';
-        modal += '<div><a href="#close" title="Close" class="close">X</a></div>';
+  // Load modal containers with employee details from the current employee listing
+  let loadModals = (list) => {
+    let modal = '';
 
-        // Employee gallery - next and previous links to navigate through employee detail windows
-        let next = i + 1;
-        let prev = i - 1;
+    $.each(list, function(i, employee) {
+      let prev = i-1;
+      let next = i+1;
 
-        if (i === 0) {
-          modal += '<a href="#openModal' + next + '" class="next">></a></div>';
-        } else if (i === 11) {
-          modal += '<div><a href="#openModal' + prev + '" class="prev"><</a>';
-        } else {
-          modal += '<div><a href="#openModal' + prev + '" class="prev"><</a>';
-          modal += '<a href="#openModal' + next + '" class="next">></a></div>';
-        }
+      modal += '<div id="openModal' + i + '" class="modalDialog"><div>';
+      modal += '<img src="' + employee.picture.large + '" class="employee-picture-details">';
+      modal += '<div class="employee-info-details">';
+      modal += '<p class="name">' + employee.name.first + ' ' + employee.name.last + '</p>';
+      modal += '<p class="email">' + employee.email + '</p>';
+      modal += '<p class="city">' + employee.location.city + '</p><hr>';
+      modal += '<p>' + employee.cell + '</p>';
+      modal += '<p>' + employee.location.street + ' ' + employee.location.city + ', ' + employee.location.state + ' ' + employee.location.postcode + '</p>';
+      modal += '<p>' + formatBirthdate(employee.dob) + '</p>';
+      modal += '<div><a href="#close" title="Close" class="close">X</a></div>';
+      modal += '<div><a href="#openModal' + prev + '" class="prev"><</a><a href="#openModal' + next + '" class="next">></a></div>';
+      modal += '</div></div>';
+      modal += '</div>';
 
-        modal += '</div></div>';
-        modal += '</div>';
+    });
 
-      });
+    $('#modals').html(modal);
 
-      $('#modals').html(modal);
-
+    // If there is only one result, remove previous/next buttons from modal window
+    if (list.length === 1) {
+      $('.prev').hide();
+      $('.next').hide();
+    } else if (list.length > 1) {
+      $('.prev').get(0).remove(); // Remove previous button link from first modal window
+      $('.next').get(list.length-1).remove(); // Remove next button link from last modal window
     }
-  });
+  }
 
-    // Filter feature - allow to filter by name or username
-    // Add input field and button
-    let $filterContainer = $('<div id="filterSection">');
-    let $searchInputField = $('<input type="search" id="search">');
-    let $filterButton = $('<input type="button" id="button" value="Filter">');
-    $($filterContainer).append($searchInputField);
-    $($filterContainer).append($filterButton);
-    $('header').append($filterContainer);
+  // Format birthdate
+  let formatBirthdate = (dob) => {
+    let dobString = dob.split('-');
+    let dobDay = dobString[2].substring(0, 2);
+    let birthday = 'Birthday: ' + dobString[1] + '/' + dobDay + '/' + dobString[0];
+    return birthday;
+  }
 
-    // Filter button event handler
-    $($filterButton).click(function() {
-      let $searchValue = $('#search').val().toUpperCase();
+  // Filter feature - allow to filter by employee name or username
+  let $filterContainer = $('<div id="filterSection">');
+  let $filterInputField = $('<input type="search" id="filter" placeholder="Filter">');
+  $($filterContainer).append($filterInputField);
+  $('header').append($filterContainer);
+
+  // Filter function displays employee info based on query
+  let filter = () => {
+    let $filterValue = $('#filter').val().toUpperCase();
+    let filteredList = [];
 
     $.each(employeeList, function(i, employee) {
       let employeeName = employee.name.first + ' ' + employee.name.last;
       let employeeUsername = employee.login.username;
 
-      // If search input matches an employee's name or username, show relevant results
-      // If search input is blank when filter is performed, then reset grid
-      if (employeeName.toUpperCase().indexOf($searchValue) > -1 && $searchValue !== '') {
-        $('td').hide();
-        $('#' + i).show();
-      } else if (employeeUsername.toUpperCase().indexOf($searchValue) > -1 && $searchValue !== '') {
-        $('td').hide();
-        $('#' + i).show();
-      } else if ($searchValue === '') {
-        $('td').show();
+      if (employeeName.toUpperCase().indexOf($filterValue) > -1 || employeeUsername.toUpperCase().indexOf($filterValue) > -1) {
+        filteredList.push(employee);
       }
-    })
-  })
+    });
+
+    displayEmployeeDirectory(filteredList); // Call function to display filteredList data
+
+    // Display No Results message if filteredList is empty
+    if (filteredList.length === 0) {
+      let noResults = '<p>No Results Found</p>';
+      $('.employees').append(noResults);
+    }
+  }
+
+  $('#filter').on('keyup', filter); // Filter field event handler
+
+  // AJAX Call to Random User Generator API to generate 12 employees
+  $.ajax({
+    url: 'https://randomuser.me/api/?results=12&seed=lovers&nat=us',
+    dataType: 'json',
+    success: function(data) {
+      getEmployeeData(data.results);
+    }
+  });
 
 });
